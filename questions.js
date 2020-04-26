fe95fb903060f5b15b4089cfa2964eeaab19e079
Binh Nguyen <Binh.dai.nguyen.97@gmail.com>
2020-04-25 21:19:54 -0700

    Created questions.js - an array with objects containing questions, choices and answers for the quiz

---
+++ questions.js
@@ -0,0 +1,17 @@
+var questionsArray = [
+    {
+        question: "Which of the folllowing is used to create a Web Page?",
+        choices:  ["HTML", "C", "DTD", "JVM"],
+        answer: "HTML"
+    },
+    {
+        question: "HTML stands for ________",
+        choices:  ["Hyper Tech Markup Language", "Hyper Text Makeup Language", "Hyper Text Markup Language", "Hippie Truck Miles Looker"],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "Which of the following is used to read HTML Code and to render Webpage?",
+        choices:  ["Web Matrix", "Web Browser", "Web Server", "Spyder Webs"],
+        answer: "Web Browser"
+    }
+]
